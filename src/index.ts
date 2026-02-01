ed3ab0e3b27a4a6ca73e51e908238125b64b802a
Zach Fedor <zachfedor@gmail.com>
2026-02-01 10:39:28 -0500

    feat: reexport types

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-import { ContractExplorer as _CE } from "./components/ContractExplorer"
-import { ContractExplorerModal as _CEM } from "./components/ContractExplorerModal"
+export { ContractExplorer } from "./components/ContractExplorer"
+export { ContractExplorerModal } from "./components/ContractExplorerModal"
+export * from "./types/types"
 export { loadContracts } from "./util/loadContracts"
-
-// Prevent contract explorer from being used in production
-export const ContractExplorer =
-	process.env.NODE_ENV === "development"
-		? _CE
-		: function () {
-				return null
-			}
-
-export const ContractExplorerModal =
-	process.env.NODE_ENV === "development"
-		? _CEM
-		: function () {
-				return null
-			}
